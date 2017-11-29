a9700d8b9ff460574b6fe1f53ffcf7242e661608
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-11-29 13:52:38 +0100

    Step 1.24: Clean module

--- src/app/selectable-list/selectable-list-module.ts
+++ src/app/selectable-list/selectable-list-module.ts
@@ -1,4 +1,3 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {SelectableItemDirective} from './directive/selectable-item/selectable-item.directive';
@@ -10,10 +9,6 @@ import {SelectableListDirective} from './directive/selectable-list/selectable-li
     SelectableItemDirective,
     SelectableListDirective,
   ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [],
   exports: [
     SelectableItemDirective,
     SelectableListDirective,
